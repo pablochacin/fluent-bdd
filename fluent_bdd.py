c0ba1c0cd8e8e7ff2cffe482d2ee4b0d6c98d7e4
Pablo <pablochacin@gmail.com>
2019-09-01 19:39:31 +0200

    WIP: Separate semanting model from DSL
    Following Martin Fowler DSL book, separate the test bdd model
    from the fluent api DSL.
    
    Signed-off-by: Pablo <pablochacin@gmail.com>

---
+++ fluent_bdd.py
@@ -0,0 +1,123 @@
+import bdd
+
+class BackgroundBuilder:
+
+    class ConditionBuilder:
+
+        def __init__(self, background, featureBld):
+            self.background = background
+            self.featureBld = featureBld
+
+        def And(self, condition, *args, **kwargs):
+            self.background.addCondition(condition, *args, **kwargs)
+            return self
+
+        def Scenario(self, title):
+            return self.featureBld.Scenario(title)
+
+     
+    def __init__(self, featureBld, background):
+        self.featureBld = featureBld
+        self.background = background
+
+    def Given(self, condition, *args, **kwargs):
+        self.background.addCondition(condition, *args, **kwargs)
+        return BackgroundBuilder.ConditionBuilder(self.background, self.featureBld)
+
+class ScenarioBuilder:
+
+    class ConditionBuilder:
+
+        def __init__(self, scenarioBld):
+           self.scenarioBld = scenarioBld
+
+        def And(self, condition, *args, **kwargs):
+            self.scenarioBld.scenario.addCondition(condition, *args, **kwargs)
+            return self
+
+        def When(self, event, *args, **kwargs):
+            self.scenarioBld.scenario.addEvent(event, *args, **kwargs)
+            return ScenarioBuilder.EventBuilder(self.scenarioBld)
+
+
+    class EventBuilder:
+
+        def __init__(self, scenarioBld):
+           self.scenarioBld = scenarioBld
+
+        def And(self, event, *args, **kwargs):
+            self.scenarioStm.scenario.addEvent(event, *args, **kwargs)
+            return self
+
+        def Then(self, clause, *args, **kwargs):
+            return ScenarioBuilder.AssertionBuilder(self.scenarioBld, clause, *args, **kwargs)
+
+    class AssertionBuilder:
+
+        def __init__(self, scenarioBld, clause, *args, **kwargs):
+            self.scenarioBld = scenarioBld
+            self.clause = clause
+            self.args = args
+            self.kwargs = kwargs
+
+        def Is(self, value):
+            self.scenarioBld.scenario.addClause(self.clause, bdd.Assertion.Is, value, *self.args, **self.kwargs)
+            return ScenarioBuilder.ClauseBuilder(self.scenarioBld)
+
+        def IsNot(self, value):
+            self.scenarioBld.scenario.addClause(self.clause, bdd.Assertion.IsNot, value, *self.args, **self.kwargs)
+            return ScenarioBuilder.ClauseBuilder(self.scenarioBld)
+
+    class ClauseBuilder:
+
+        def __init__(self, scenarioBld):
+           self.scenarioBld = scenarioBld
+
+        def And(self, clause, *args, **kwargs):
+            return Scenariostatement.AssertionBuilder(self.scenarioBld, clause, *args, **kwargs)
+
+        def Scenario(self, title):
+            return self.scenarioBld.featureBld.Scenario(title)
+
+        def Test(self):
+            return self.scenarioBld.featureBld.Test()
+
+    def __init__(self, featureBld, scenario):
+        self.featureBld = featureBld
+        self.scenario = scenario
+
+    def Given(self, condition, *args, **kwargs):
+        self.scenario.addCondition(condition, *args, **kwargs)
+        return ScenarioBuilder.ConditionBuilder(self)
+
+    def When(self, event, *args, **kwargs):
+        self.scenario.addEvent(event, *args, **kwargs)
+        return ScenarioBuilder.EventBuilder(self)
+
+    def WithValues(self, arg_names, values):
+        self.scenario.setValues(arg_names, values)
+        return self
+
+class FeatureBuilder:
+
+    def __init__(self, name):
+        self.feature= bdd.Feature(name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self,*args):
+        return self.feature.test()
+
+    def Background(self):
+        b = bdd.Feature.Background()
+        self.feature.setBackground(b)
+        return BackgroundBuilder(self,b)
+
+    def Scenario(self, title):
+        s = bdd.Scenario(self.feature, title)
+        self.feature.addScenario(s)
+        return ScenarioBuilder(self, s)
+
+    def Test(self):
+        return self.feature.test()
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from bdd import Feature
+from fluent_bdd import FeatureBuilder
 
 def echo(a):
     return a
@@ -25,55 +25,56 @@ def main():
 
     sut = SUT()
 
-    with Feature("My feature") as f:        
-        f.Scenario("Success Test") \
-            .Given(sut.f0).When(sut.f2, 1, 2) \
-            .Then(echo, True).Is(True) 
+    with FeatureBuilder("My feature") as f:        
+            f.Scenario("Success Test") \
+                .Given(sut.f0).When(sut.f2, 1, 2) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("With Values") \
-            .WithValues(('a', 'b','c'), [(1,2,True), (2,3,False)]) \
-            .Given(sut.f0) \
-            .When(sut.f2, 'a', '{b}')  \
-            .Then(echo, True).Is('{c}') \
+            f.Scenario("With Values") \
+                .WithValues(('a', 'b','c'), [(1,2,True), (2,3,False)]) \
+                .Given(sut.f0) \
+                .When(sut.f2, 'a', '{b}')  \
+                .Then(echo, True).Is('{c}')
+     
+            f.Scenario("No Conditions") \
+                .When(sut.f2, 1, 2) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("No Conditions") \
-            .When(sut.f2, 1, 2) \
-            .Then(echo, True).Is(True) 
+            f.Scenario("Multiple Conditions") \
+                .Given(sut.f0) \
+                .And(sut.f1,'a') \
+                .When(sut.f2, 1, 2) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("Multiple Conditions") \
-            .Given(sut.f0) \
-            .And(sut.f1,'a') \
-            .When(sut.f2, 1, 2) \
-            .Then(echo, True).Is(True) 
+            f.Scenario("Kwargs") \
+                .When(sut.fkw, a=1, b=2, c=3) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("Kwargs") \
-            .When(sut.fkw, a=1, b=2, c=3) \
-            .Then(echo, True).Is(True) 
+            f.Scenario("Invalid Kwargs") \
+                .When(sut.fkw, a=1, b=2, d=3) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("Invalid Kwargs") \
-            .When(sut.fkw, a=1, b=2, d=3) \
-            .Then(echo, True).Is(True) 
+            f.Scenario("Failing Assertion") \
+                .Given(sut.f0).When(sut.f2, 1, 2) \
+                .Then(echo, False).Is(True) \
 
-        f.Scenario("Failing Assertion") \
-            .Given(sut.f0).When(sut.f2, 1, 2) \
-            .Then(echo, False).Is(True) \
+            f.Scenario("Failing condition") \
+                .Given(sut.f0) \
+                .When(sut.fail) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("Failing condition") \
-            .Given(sut.f0) \
-            .When(sut.fail) \
-            .Then(echo, True).Is(True) 
+            f.Scenario("Wrong function arity") \
+                .Given(sut.f0) \
+                .When(sut.f1,1,2) \
+                .Then(echo, True).Is(True) 
 
-        f.Scenario("Wrong function arity") \
-            .Given(sut.f0) \
-            .When(sut.f1,1,2) \
-            .Then(echo, True).Is(True) 
 
-    Feature("My feature with background") \
-        .Background().Given(sut.f0).And(sut.f2,'a','b') \
-        .Scenario("Copying Conditions") \
-            .When(sut.f2, 1, 2) \
-            .Then(echo, True).Is(True) \
-        .Test()
+    FeatureBuilder("My feature with background") \
+            .Background().Given(sut.f0).And(sut.f2,'a','b') \
+            .Scenario("Copying Conditions") \
+                .When(sut.f2, 1, 2) \
+                .Then(echo, True).Is(True) \
+            .Test()
 
 if __name__ == "__main__":
     main()
